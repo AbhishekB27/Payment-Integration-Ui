8fd3dfa1002b48cb4c4e0ef99e8f28d709f7997d
AbhishekB27 <akstyles27@gmail.com>
2022-12-09 18:04:21 +0530

    UI Design Of Payment Integration

---
+++ src/Components/MyApp.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import PaymentM from "./PaymentM";
+
+const MyApp = () => {
+  return (
+    <div className="bg-purple-500 p-2 grid place-items-center min-h-screen">
+      <PaymentM />
+    </div>
+  );
+};
+
+export default MyApp;
